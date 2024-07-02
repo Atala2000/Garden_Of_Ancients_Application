b3579b6c50cc5d025dbbc3f3f5fb61f87f5614d2
Atala2000 <atalakidi@gmail.com>
2024-07-02 22:26:03 +0300

    chore: Remove unused routes file

--- server/src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { calcPrice } from "../controllers/controller";
-
-const router = express.Router();
-
-router.get('/getPrices', calcPrice);
-
-export default router;
