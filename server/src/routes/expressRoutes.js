70f06d86e63aa28e74c5405afd85baa4b52b58f0
Atala2000 <atalakidi@gmail.com>
2024-07-06 13:34:41 +0300

    chore: Add PayPal order creation and capture endpoints

--- server/src/routes/expressRoutes.js
+++ server/src/routes/expressRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { Login, Logout, SignUp, calcPrice } from "../controllers/controller.js";
+import { captureOrder, createOrder } from "../utils/paypalUtils.js";
 
 const router = express.Router();
 
@@ -7,5 +8,7 @@ router.post('/getPrices', calcPrice);
 router.post('/signup', SignUp);
 router.post('/login', Login);
 router.get('/logout', Logout);
+router.post('/create-order', createOrder)
+router.post('/capture-order', captureOrder)
 
 export default router;
