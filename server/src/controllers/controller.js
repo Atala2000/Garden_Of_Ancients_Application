13133cb7039df74d4cfceb6fe0b8634adbd3657c
Atala2000 <atalakidi@gmail.com>
2024-07-06 15:16:13 +0300

    fix: Added resolve to merge issue

--- server/src/controllers/controller.js
+++ server/src/controllers/controller.js
@@ -133,3 +133,25 @@ export const Logout = async (req, res) => {
     });
 }
 
+export const createOrderController = async (req, res) => {
+    const { amount } = req.body;
+    try {
+      const orderData = await createOrder(amount);
+      res.json(orderData);
+    } catch (error) {
+      console.error('Error creating order:', error);
+      res.status(500).send('Error creating order');
+    }
+  };
+  
+export const captureOrderController = async (req, res) => {
+    const { orderID } = req.body;
+    try {
+      const captureData = await captureOrder(orderID);
+      res.json(captureData);
+    } catch (error) {
+      console.error('Error capturing order:', error);
+      res.status(500).send('Error capturing order');
+    }
+  };
+  
